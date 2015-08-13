b462a075e15fb79536022cd308f33c5497e4a8af
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2015-08-13 20:17:53 -0300

    day 32
    .

--- code/handmadehero.h
+++ code/handmadehero.h
@@ -1,7 +1,6 @@
 #if !defined(HANDMADEHERO_H)
 
-#include <math.h>
-#include <stdint.h>
+#include "handmade_platform.h"
 
 #define internal static 
 #define local_persist static 
@@ -39,37 +38,15 @@ typedef double real64;
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 
-/*
-  NOTE(casey): Services that the platform layer provides to the game
-*/
-
-struct thread_context
+inline uint32
+SafeTruncateUInt64(uint64 Value)
 {
-    int Placeholder;
-};
+    // TODO(casey): Defines for maximum values
+    Assert(Value <= 0xFFFFFFFF);
+    uint32 Result = (uint32)Value;
+    return(Result);
+}
 
-#if HANDMADE_INTERNAL
-/* IMPORTANT(casey):
-
-   These are NOT for doing anything in the shipping game - they are
-   blocking and the write doesn't protect against lost data!
-*/
-struct debug_read_file_result
-{
-    uint32 ContentsSize;
-    void *Contents;
-};
-
-#define DEBUG_PLATFORM_FREE_FILE_MEMORY(name) void name(thread_context *Thread, void *Memory)
-typedef DEBUG_PLATFORM_FREE_FILE_MEMORY(debug_platform_free_file_memory);
-
-#define DEBUG_PLATFORM_READ_ENTIRE_FILE(name) debug_read_file_result name(thread_context *Thread, char *Filename)
-typedef DEBUG_PLATFORM_READ_ENTIRE_FILE(debug_platform_read_entire_file);
-
-#define DEBUG_PLATFORM_WRITE_ENTIRE_FILE(name) bool32 name(thread_context *Thread, char *Filename, uint32 MemorySize, void *Memory)
-typedef DEBUG_PLATFORM_WRITE_ENTIRE_FILE(debug_platform_write_entire_file);
-
-#endif
 
 /*
   NOTE(casey): Services that the game provides to the platform layer.
@@ -81,16 +58,6 @@ typedef DEBUG_PLATFORM_WRITE_ENTIRE_FILE(debug_platform_write_entire_file);
 // TODO(casey): In the future, rendering _specifically_ will become a three-tiered abstraction!!!
 
 
-// TODO(casey): Is this ever necessary?
-struct raw_position
-{
-    int32 TileMapX;
-    int32 TileMapY;
-
-    // NOTE(casey): Tile-map relative X and Y
-    real32 X;
-    real32 Y;
-};
 
 struct canonical_position
 {
@@ -119,133 +86,12 @@ struct tile_map
     uint32 *Tiles;
 };
 
-struct game_Off_Screen_Buffer
-{
-	void *Memory;
-	int Width;
-	int Height;
-	int Pitch;
-	int BytesPerPixel;
-};
-
-inline uint32
-SafeTruncateUInt64(uint64 Value)
-{
-    // TODO(casey): Defines for maximum values
-    Assert(Value <= 0xFFFFFFFF);
-    uint32 Result = (uint32)Value;
-    return(Result);
-}
-
-struct game_sound_output_buffer
-{
-	int sampleCount;
-	real32 tSine;
-	int32 samplesPerSecond;
-	int16 *samples;
-	
-};
-
-struct game_button_state
-{
-    int HalfTransitionCount;
-    bool32 EndedDown;
-};
-
-struct game_controller_input
-{
-    bool32 IsConnected;
-    bool32 IsAnalog;    
-    real32 StickAverageX;
-    real32 StickAverageY;
-	real32 RightStickAverageX;
-    real32 RightStickAverageY;
-    
-    union
-    {
-        game_button_state Buttons[12];
-        struct
-        {
-            game_button_state MoveUp;
-            game_button_state MoveDown;
-            game_button_state MoveLeft;
-            game_button_state MoveRight;
-            
-            game_button_state ActionUp;
-            game_button_state ActionDown;
-            game_button_state ActionLeft;
-            game_button_state ActionRight;
-            
-            game_button_state LeftShoulder;
-            game_button_state RightShoulder;
-
-            game_button_state Back;
-            game_button_state Start;
-
-            // NOTE(casey): All buttons must be added above this line
-            
-            game_button_state Terminator;
-        };
-    };
-};
-
-
-struct ButtonActions
-{
-	bool32 Up;
-	bool32 Down;
-	bool32 Left;
-	bool32 Right; 
-	bool32 ButA;
-	bool32 ButB;
-	bool32 ButX;
-	bool32 ButY;
-	bool32 LeftSh;
-	bool32 RigtSh;
-	
-	int16 StickX;
-	int16 StickY;
-};
-
-struct game_input
-{
-    game_button_state MouseButtons[5];
-    int32 MouseX, MouseY, MouseZ;
-	
-	real32 dtForFrame;
-	
-	game_controller_input Controllers[5];
-};
-
-struct game_memory
-{
-    bool32 IsInitialized;
-
-    uint64 PermanentStorageSize;
-    void *PermanentStorage; // NOTE(casey): REQUIRED to be cleared to zero at startup
-
-    uint64 TransientStorageSize;
-    void *TransientStorage; // NOTE(casey): REQUIRED to be cleared to zero at startup
-	
-	debug_platform_free_file_memory *DEBUGPlatformFreeFileMemory;
-	debug_platform_read_entire_file *DEBUGPlatformReadEntireFile;
-    debug_platform_write_entire_file *DEBUGPlatformWriteEntireFile;
-};
-
-#define GAME_UPDATE_AND_RENDER(name) void name(thread_context *Thread, game_memory *Memory, game_input *Input, game_Off_Screen_Buffer *Buffer)
-typedef GAME_UPDATE_AND_RENDER(game_update_and_render);
-
-// NOTE(casey): At the moment, this has to be a very fast function, it cannot be
-// more than a millisecond or so.
-// TODO(casey): Reduce the pressure on this function's performance by measuring it
-// or asking about it, etc.
-#define GAME_GET_SOUND_SAMPLES(name) void name(thread_context *Thread, game_memory *Memory, game_sound_output_buffer *SoundBuffer)
-typedef GAME_GET_SOUND_SAMPLES(game_get_sound_samples);
-
 struct world
 {
 	real32 TileSideInMeters;
     int32 TileSideInPixels;
+	
+	real32 MetersToPixels;
 	
     int32 CountX;
     int32 CountY;
@@ -262,12 +108,7 @@ struct world
 
 struct game_state
 {
-// TODO(casey): Player state should be canonical position now?
-    int32 PlayerTileMapX;
-    int32 PlayerTileMapY;
-    
-    real32 PlayerX;
-    real32 PlayerY;
+	canonical_position PlayerP;
 };
 
 
