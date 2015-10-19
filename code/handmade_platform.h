7e8774cab91b1e831ad02303ec02f237463e0594
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2015-10-18 22:26:46 -0200

    day 34
    .

--- code/handmade_platform.h
+++ code/handmade_platform.h
@@ -23,7 +23,7 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
+typedef size_t memory_index;
 typedef float real32;
 typedef double real64;
 
--- code/handmadehero.h
+++ code/handmadehero.h
@@ -36,6 +36,8 @@ typedef double real64;
 #define Gigabytes(Value) (Megabytes(Value)*1024LL)
 #define Terabytes(Value) (Gigabytes(Value)*1024LL)
 
+#define Pi32 3.14159265359f
+
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 
 inline uint32
@@ -72,76 +74,27 @@ struct canonical_position
     real32 TileRelX;
     real32 TileRelY;
 };
+#include "handmade_intrinsics.h"
+#include "handmade_tile.h"
 
-struct tile_map
+struct memory_arena
 {
-    int32 CountX;
-    int32 CountY;
-    
-    real32 UpperLeftX;
-    real32 UpperLeftY;
-    real32 TileWidth;
-    real32 TileHeight;
-
-    uint32 *Tiles;
-};
-struct tile_chunk
-{
-    uint32 *Tiles;
+    memory_index Size;
+    uint8 *Base;
+    memory_index Used;
 };
 
 struct world
-{
-    uint32 ChunkShift;
-    uint32 ChunkMask;
-    uint32 ChunkDim;
-    
-    real32 TileSideInMeters;
-    int32 TileSideInPixels;
-    real32 MetersToPixels;
-
-    // TODO(casey): Beginner's sparseness
-    int32 TileChunkCountX;
-    int32 TileChunkCountY;
-    
-    tile_chunk *TileChunks;
-};
-
-struct tile_chunk_position
-{
-    uint32 TileChunkX;
-    uint32 TileChunkY;
-
-    uint32 RelTileX;
-    uint32 RelTileY;
-};
-
-
-struct world_position
-{
-    /* TODO(casey):
-
-       Take the tile map x and y
-       and the tile x and y
-
-       and pack them into single 32-bit values for x and y
-       where there is some low bits for the tile index
-       and the high bits are the tile "page"
-
-       (NOTE we can eliminate the need for floor!)
-    */
-    uint32 AbsTileX;
-    uint32 AbsTileY;
-
-    // TODO(casey): Should these be from the center of a tile?
-    // TODO(casey): Rename to offset X and Y
-    real32 TileRelX;
-    real32 TileRelY;
+{    
+    tile_map *TileMap;
 };
 
 struct game_state
 {
-	world_position PlayerP;
+	memory_arena WorldArena;
+    world *World;
+    
+    tile_map_position PlayerP;
 };
 
 
