f3fadfbfade23564f630f54141f6fdd1390a635b
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2016-07-02 00:51:00 -0300

    Day 38
    Alfa blend BMP

--- code/handmade_platform.h
+++ code/handmade_platform.h
@@ -10,6 +10,37 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+//
+// NOTE(casey): Compilers
+//
+    
+#if !defined(COMPILER_MSVC)
+#define COMPILER_MSVC 0
+#endif
+    
+#if !defined(COMPILER_LLVM)
+#define COMPILER_LLVM 0
+#endif
+
+#if !COMPILER_MSVC && !COMPILER_LLVM
+#if _MSC_VER
+#undef COMPILER_MSVC
+#define COMPILER_MSVC 1
+#else
+// TODO(casey): Moar compilerz!!!
+#undef COMPILER_LLVM
+#define COMPILER_LLVM 1
+#endif
+#endif
+
+#if COMPILER_MSVC
+#include <intrin.h>
+#endif
+    
+//
+// NOTE(casey): Types
+//
     
 #include <stdint.h>
 
@@ -66,7 +97,6 @@ struct game_Off_Screen_Buffer
 	int Pitch;
 	int BytesPerPixel;
 };
-
 
 struct game_sound_output_buffer
 {
