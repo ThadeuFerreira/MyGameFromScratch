1a29abdaeb1b7c20cc8fc3c619329da33ebb802f
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2016-07-05 21:03:41 -0300

    Revert "Revert "Day 42""
    This reverts commit dcaa62774899b54fd5c46b34d6a04314ef954f77.

--- code/handmadehero.h
+++ code/handmadehero.h
@@ -100,6 +100,8 @@ PushSize_(memory_arena *Arena, memory_index Size)
     
     return(Result);
 }
+
+#include "handmade_math.h"
 #include "handmade_intrinsics.h"
 #include "handmade_tile.h"
 struct world
