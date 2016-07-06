3bd22061ef95aa216491a41a7da71ad12f1b13e4
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2016-07-05 21:03:46 -0300

    Revert "Revert "day 43""
    This reverts commit e4ddfd6003f12f4e6361b5aad869bf31ce776e75.

--- code/handmadehero.h
+++ code/handmadehero.h
@@ -2,54 +2,6 @@
 
 #include "handmade_platform.h"
 
-#define internal static 
-#define local_persist static 
-#define global_variable static
-
-#define PIXEL_BIT_COUNT 32
-#define BYTES_PER_PIXEL 4
-#define Pi32 3.14159265359f
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef int32 bool32;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef float real32;
-typedef double real64;
-
-#if HANDMADE_SLOW
-// TODO(casey): Complete assertion macro - don't worry everyone!
-#define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
-#else
-#define Assert(Expression)
-#endif
-
-#define Kilobytes(Value) ((Value)*1024LL)
-#define Megabytes(Value) (Kilobytes(Value)*1024LL)
-#define Gigabytes(Value) (Megabytes(Value)*1024LL)
-#define Terabytes(Value) (Gigabytes(Value)*1024LL)
-
-#define Pi32 3.14159265359f
-
-#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
-
-inline uint32
-SafeTruncateUInt64(uint64 Value)
-{
-    // TODO(casey): Defines for maximum values
-    Assert(Value <= 0xFFFFFFFF);
-    uint32 Result = (uint32)Value;
-    return(Result);
-}
-
-
 /*
   NOTE(casey): Services that the game provides to the platform layer.
   (this may expand in the future - sound on separate thread, etc.)
@@ -60,20 +12,6 @@ SafeTruncateUInt64(uint64 Value)
 // TODO(casey): In the future, rendering _specifically_ will become a three-tiered abstraction!!!
 
 
-
-struct canonical_position
-{
-    int32 TileMapX;
-    int32 TileMapY;
-
-    int32 TileX;
-    int32 TileY;
-
-    // NOTE(casey): This is tile-relative X and Y
-    // TODO(casey): These are still in pixels... :/
-    real32 TileRelX;
-    real32 TileRelY;
-};
 struct memory_arena
 {
     memory_index Size;
@@ -132,7 +70,7 @@ struct game_state
     
     tile_map_position CameraP;
     tile_map_position PlayerP;
-
+    v2 dPlayerP;
     loaded_bitmap Backdrop;
     uint32 HeroFacingDirection;
     hero_bitmaps HeroBitmaps[4];
@@ -148,15 +86,6 @@ struct game_state
    These are NOT for doing anything in the shipping game - they are
    blocking and the write doesn't protect against lost data!
 */
-
-
-inline game_controller_input *GetController(game_input *Input, int unsigned ControllerIndex)
-{
-    Assert(ControllerIndex < ArrayCount(Input->Controllers));
-    
-    game_controller_input *Result = &Input->Controllers[ControllerIndex];
-    return(Result);
-}
 
 #endif
 
